10cd51aaa77a6a3f8673e47db5cd28a771c28680
sandhyaba <86476805+sandhyaba@users.noreply.github.com>
2023-05-03 12:13:38 +0530

    Update config.js

--- src /config/config.js
+++ src /config/config.js
@@ -1,1 +1,14 @@
-
+const mongoose = require("mongoose");
+const dbSrv = "mongodb://127.0.0.1:27017/Project_2";
+mongoose.set('strictQuery', false);
+db = mongoose.connect(
+    dbSrv,{ useNewUrlParser: true ,useUnifiedTopology: true},
+    (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Database connected");
+        }
+    }
+);
+module.exports = { db };
